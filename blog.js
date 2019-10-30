2d905939b300b70223d07eea4c412b19338fc4ff
choupette <choupette@localhost.localdomain>
2019-10-31 00:30:53 +0700

    Fixed navigation bar and more

--- blog.js
+++ blog.js
@@ -1,4 +1,29 @@
+// test to add clicking function to my ul
+$(document).ready(function(){
+  $('#toggle-area').click(function(){
+    $('#show-area').toggleClass("open");
+  });
+});
 
+$(document).ready(function(){
+  $('#toggle-area').click(function(){
+    if (! $('#show-area').hasClass('open')){
+      $('#show-area').addClass('open');
+    }
+    else {
+      $('#show-area').removeClass('open');
+    }
+  });
+});
+// end 1
+
+
+// test to add clicking fonction to my ul ------navbar
+const selectElement = (element) => document.querySelector(element);
+selectElement('navbar-toggler').addEventListener('click', () => {
+    selectElement('navbar-nav').classList.toggle('active');
+});
+//end 
 
 brightness_4
 // JavaScript code 

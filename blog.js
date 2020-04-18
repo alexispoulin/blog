258ac20e8af65585ddb1dc648490698510ccd456
alexispoulin <alexispoulin20@gmail.com>
2020-04-18 12:32:30 -0400

    Final update

--- blog.js
+++ blog.js
@@ -1,13 +1,13 @@
 // test to add clicking function to my ul
-$(document).ready(function(){
-  $('#toggle-area').click(function(){
+$(document).ready(function () {
+  $('#toggle-area').click(function () {
     $('#show-area').toggleClass("open");
   });
 });
 
-$(document).ready(function(){
-  $('#toggle-area').click(function(){
-    if (! $('#show-area').hasClass('open')){
+$(document).ready(function () {
+  $('#toggle-area').click(function () {
+    if (!$('#show-area').hasClass('open')) {
       $('#show-area').addClass('open');
     }
     else {
@@ -21,24 +21,25 @@ $(document).ready(function(){
 // test to add clicking fonction to my ul ------navbar
 const selectElement = (element) => document.querySelector(element);
 selectElement('navbar-toggler').addEventListener('click', () => {
-    selectElement('navbar-nav').classList.toggle('active');
+  selectElement('navbar-nav').classList.toggle('active');
 });
 //end 
 
 brightness_4
 // JavaScript code 
-function search_animal() { 
-    let input = document.getElementById('searchbar').value 
-    input=input.toLowerCase(); 
-    let x = document.getElementsByClassName('blog'); 
-      
-    for (i = 0; i < x.length; i++) {  
-        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
-            x[i].style.display="none"; 
-        } 
-        else { 
-            x[i].style.display="list-item";                  
-        } 
-    } 
-} 
+function search_animal() {
+  let input = document.getElementById('searchbar').value
+  input = input.toLowerCase();
+  let x = document.getElementsByClassName('blog');
 
+  for (i = 0; i < x.length; i++) {
+    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+      x[i].style.display = "none";
+    }
+    else {
+      x[i].style.display = "list-item";
+    }
+  }
+}
+
+// Special thanks to professional JS - Alex Montambeault 
